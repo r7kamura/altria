6b37d21fa87b4d497f708d5aa907473c9c0fb55b
Ryo Nakamura <r7kamura@gmail.com>
2013-05-31 18:10:44 +0900

    Usage

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # Magi
+Yet another an open source continuous integration server.
 
-## Installation
+## Easy mode
+Simply invokes built-in commands.
+
 ```
 $ gem install magi
 $ magi setup
-$ magi start
+$ magi worker
+$ magi web
+```
+
+## Hard mode
+Magi is just a rails application. Feel free to do what you want.
+
+```
+$ git clone git@github.com:r7kamura/magi.git
+$ cd magi
+$ bundle install
+$ bundle exec sidekiq
+$ ./bin/magi web
 ```
 
 ## ToDo
 * magi setup command
 * magi start command
 * worker process
-* web interface
-* web api
+* web interface application/json
+* web interface text/html
--- lib/magi/command.rb
+++ lib/magi/command.rb
@@ -1,3 +1,19 @@
+# Provides command line interface for Magi.
+#
+# Examples
+#
+#   # Takes ARGV and invokes a job.
+#   Magi::Command.call(ARGV)
+#
+#   # Invokes "setup" command to set up your database.
+#   Magi::Command.call(["setup"])
+#
+#   # Invokes "web" command to start a HTTP server.
+#   Magi::Command.call(["web"])
+#
+#   # Invokes "worker" command to start a worker for your jobs.
+#   Magi::Command.call(["worker"])
+#
 module Magi
   class Command
     def self.call(*args)
@@ -9,7 +25,12 @@ module Magi
     end
 
     def call
-      puts name # for debug
+      case name
+      when "setup"
+      when "start"
+      when "worker"
+      else
+      end
     end
 
     private
