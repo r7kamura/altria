0ee136de4fe9881c2cd9405f04f4ad38f9ff516b
Ryo Nakamura <r7kamura@gmail.com>
2013-05-31 16:39:20 +0900

    Initial commit
    $ rails new magi --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magi::Application.config.secret_token = '52e56b4c387efb5ac8eb96c71963f58de5432808e97bc5d971ea8e04997bab457094ed4da841829ce98933564259a213dd249fb2a1079bd895e703eebfc4e8a2'
