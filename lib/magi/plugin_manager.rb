7d674fc69d1e5d6ab2944b332e860128aa0299dc
Ryo Nakamura <r7kamura@gmail.com>
2013-06-12 01:32:01 +0900

    Change plugin directory

--- lib/magi/plugin_manager.rb
+++ lib/magi/plugin_manager.rb
@@ -1,11 +1,11 @@
 module Magi
   class PluginManager
     def plugins_directory
-      @plugins_directory ||= Pathname.new("plugins")
+      @plugins_directory ||= Rails.root + "plugins"
     end
 
     def load_plugins
-      Dir.glob("#{Dir.pwd}/#{plugins_directory}/*/*.rb").sort.each do |path|
+      Dir.glob("#{plugins_directory}/*/*.rb").sort.each do |path|
         require path
       end
     end
