2d52932daafe85683db82c345aa44099d7926e4e
Ryo Nakamura <r7kamura@gmail.com>
2013-06-12 01:28:50 +0900

    Create Git plugin

--- plugins/git/git.rb
+++ plugins/git/git.rb
@@ -1,47 +1,85 @@
 require "open3"
-require "pathname"
 
-class Repository
-  delegate :exist?, to: :directory
+module Magi
+  class Repository
+    delegate :exist?, to: :directory
 
-  attr_reader :job
+    attr_reader :job
 
-  def initialize(job)
-    @job = job
-  end
+    def initialize(job)
+      @job = job
+    end
 
-  def directory
-    @directory ||= Pathname.new("jobs/#{job.name}/repository")
-  end
+    def directory
+      @directory ||= Pathname.new("jobs/#{job.id}")
+    end
 
-  def clone
-    directory.parent.mkpath
-    system("git clone #{job.repository_url} #{directory}")
-  end
+    def clone
+      validate_existence_of_git_url
+      command("git clone #{job.git_url} #{directory}")
+    end
 
-  def update
-    system("cd #{directory} && git pull")
-  end
+    def update
+      clone unless exist?
+      command("cd #{directory} && git pull")
+    end
 
-  def revision
-    Open3.capture3("cat #{directory}/.git/refs/heads/git")[0].strip
-  end
+    def revision
+      command("cd #{directory} && git rev-parse HEAD").rstrip
+    end
 
-  def updated?
-    revision != job.last_finished_build.revision
-  end
+    def updated?
+      revision != job.last_finished_build.try(:revision)
+    end
 
-  def system(command)
-    super(command, out: IO::NULL, err: IO::NULL)
+    private
+
+    def command(script)
+      Open3.capture3(script)[0]
+    end
+
+    def mkdir
+      directory.parent.mkpath
+    end
+
+    def validate_existence_of_git_url
+      raise GitUrlNotFound unless job.git_url
+    end
+
+    class GitUrlNotFound < StandardError
+      def message
+        "You must set `git_url`"
+      end
+    end
   end
 end
 
 Job.class_eval do
+  property(:git_url)
+
   def repository
-    @repository ||= Repository.new(self)
+    @repository ||= Magi::Repository.new(self)
   end
 end
 
 Build.class_eval do
   property(:revision)
+
+  def update_revision
+    self.revision = job.repository.revision
+    save
+  end
+end
+
+Job.before_hook do
+  if git_url.present?
+    repository.update
+    repository.updated?
+  end
+end
+
+Job.after_hook do
+  if git_url.present?
+    current_build.update_revision
+  end
 end
