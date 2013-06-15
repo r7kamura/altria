62af5d9e9b824eecf6a4e9d8917f94b82113ac2f
Ryo Nakamura <r7kamura@gmail.com>
2013-06-15 21:56:16 +0900

    Handle ./plugins as plugins directory too

--- lib/magi/plugin_manager.rb
+++ lib/magi/plugin_manager.rb
@@ -1,13 +1,21 @@
 module Magi
   class PluginManager
-    def plugins_directory
-      @plugins_directory ||= Rails.root + "plugins"
+    def plugins_directories
+      [Rails.root + "plugins", Pathname.pwd + "plugins"].uniq
     end
 
     def load_plugins
-      Dir.glob("#{plugins_directory}/*/*.rb").sort.each do |path|
-        require path
-      end
+      plugin_filepaths.each {|path| require path }
+    end
+
+    private
+
+    def plugin_filepaths
+      Dir.glob("#{plugins_directories_pattern}/*/*.rb").sort
+    end
+
+    def plugins_directories_pattern
+      plugins_directories.join(",")
     end
   end
 end
