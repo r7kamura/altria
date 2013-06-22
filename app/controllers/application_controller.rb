c9dc74efef14a8999d3641a046352efa48139eeb
Ryo Nakamura <r7kamura@gmail.com>
2013-06-22 14:03:35 +0900

    Refacto app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,9 @@ class ApplicationController < ActionController::Base
 
   respond_to :html, :json
 
-  rescue_from WeakParameters::ValidationError do
-    head 400
-  end
-
   self.responder = Magi::Responder
 
-  private
-
-  def require_resource_params
-    head 400 unless resource_params
-  end
-
-  def resource_name
-    self.class.name.split("::").last.sub(/Controller$/, "").singularize.underscore
-  end
-
-  def resource_params
-    params[resource_name]
+  rescue_from WeakParameters::ValidationError do
+    head 400
   end
 end
