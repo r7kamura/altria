0c6e15531f5a31bdad256c1de90799e7b8935880
Ryo Nakamura <r7kamura@gmail.com>
2013-06-23 20:05:55 +0900

    Put a hook point into jobs#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,17 @@ class ApplicationController < ActionController::Base
   rescue_from WeakParameters::ValidationError do
     head 400
   end
+
+  # Cache to use `view_context.content_for` from controller.
+  # This hack is nice to hook view_context from a plugin.
+  #
+  # Examples
+  #
+  #   JobsController.before_filter do
+  #     view_context.content_for :jobs_show, "This is a footer."
+  #   end
+  #
+  def view_context
+    @view_context ||= super
+  end
 end
