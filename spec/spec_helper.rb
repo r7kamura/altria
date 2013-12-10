dc3eca9d072e23dde2823e67166aee3e4645665f
Ryo Nakamura <r7kamura@gmail.com>
2013-12-11 00:04:26 +0900

    Clean up code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,20 +13,11 @@ require "rspec/rails"
 require "rspec/autorun"
 
 RSpec.configure do |config|
-  # If you"re not using ActiveRecord, or you"d prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
-  # Enables :focus metadata filter.
   config.filter_run :focus
+  config.infer_base_class_for_anonymous_controllers = false
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.use_transactional_fixtures = true
 
   config.include RSpec::JsonMatcher, type: :request
   config.include ResponseCodeMatchers, type: :request
